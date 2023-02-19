fbbfbcab3ad1054992ab198d2481b8d4a6fe44e6
gmktdev <gmktdev@gmail.com>
2023-02-18 23:44:08 -0300

    tercer commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 fetch('./data.json')
     .then((response) => response.json())
-    .then((json) => console.log("primer console.log",json));
+    .then((json) => console.log("primer console.log "+json));
 
 const obj = JSON.parse(json);
 
