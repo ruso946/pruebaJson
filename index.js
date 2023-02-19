434168149d18f5e337d5099b1042ae6d89529e2c
gmktdev <gmktdev@gmail.com>
2023-02-19 00:17:55 -0300

    cambio titulo

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// let plan = '[{"plan":"210","valor":"1200"},'+
-//             '{"plan":"310","valor":"1300"},'+
-//             '{"plan":"410","valor":"1400"},'+
-//             '{"plan":"510","valor":"1500"}]';
+let plan = '[{"plan":"210","valor":"1200"},'+
+             '{"plan":"310","valor":"1300"},'+
+             '{"plan":"410","valor":"1400"},'+
+             '{"plan":"510","valor":"1500"}]';
 
 
 //import plan from "./data.json" assert { type: 'JSON' };
 
 fetch('./data.json')
     .then((response) => response.json())
-    .then((json) => console.log("primer console.log "+json));
-
-const obj = JSON.parse(json);
-
-console.log("segundo console.log",obj[1].plan);
-console.log("tercer console.log",obj[2].valor);
+    .then((json) => console.log("primer console.log ",json));
+    const obj = JSON.parse(plan);
+    console.log("primer console.log ",plan);
+    console.log("primer console.log pasado a objeto ",obj);
+    console.log("segundo console.log",obj[1].plan);
+    console.log("tercer console.log",obj[2].valor);
